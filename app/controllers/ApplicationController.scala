4f2a69a1ab4fe7c1a7b4e5f4a73082b43d8336cc
Elliot Wright <wright.elliot@gmail.com>
2016-11-07 16:19:03 +0000

    Database working.

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,41 @@
+package controllers
+
+import models.{User, UserForm}
+import play.api.mvc._
+import services.UserService
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+
+/**
+ * ApplicationController
+ *
+ * @author Elliot Wright <elliot@elliotwright.co>
+ */
+class ApplicationController extends Controller {
+  def index = Action.async { implicit request =>
+    UserService.listAllUsers.map(users => {
+      Ok(views.html.index(UserForm.form, users))
+    })
+  }
+
+  def addUser() = Action.async { implicit request =>
+    UserForm.form.bindFromRequest.fold(
+      // On error
+      errorForm => Future.successful(Ok(views.html.index(errorForm, Seq.empty[User]))),
+      // On success
+      data => {
+        val newUser = User(0, data.firstName, data.lastName, data.mobile, data.email)
+        UserService.addUser(newUser).map(res => {
+          Redirect(routes.ApplicationController.index())
+        })
+      })
+  }
+
+  def deleteUser(id: Long) = Action.async { implicit request =>
+    UserService.deleteUser(id).map(res => {
+      Redirect(routes.ApplicationController.index())
+    })
+  }
+}
--- build.sbt
+++ build.sbt
@@ -7,9 +7,13 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  jdbc,
   cache,
   ws,
+
+  "com.typesafe.play" %% "play-slick" % "2.0.0" % Compile,
+  "com.typesafe.play" %% "play-slick-evolutions" % "2.0.0" % Compile,
+  "mysql" % "mysql-connector-java" % "5.1.40" % Compile,
+
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
 )
 
